b75daca7c807504d5e0a8969460765a126374429
Sabina Maria <p_sabinamaria@yahoo.com>
2021-03-24 08:38:10 +0100

    ShoppingListItem:add Swagger2 annotations

--- src/main/java/com/sabina/member/serv/model/ShoppingListItem.java
+++ src/main/java/com/sabina/member/serv/model/ShoppingListItem.java
@@ -2,19 +2,52 @@ package com.sabina.member.serv.model;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class ShoppingListItem {
-
-	private Integer id;
+	@ApiModelProperty(notes = "Shopping List Item ID", dataType = "varchar",  position = 0) 
+	@JsonProperty("id")
+	private String id;
+	
+	@ApiModelProperty(notes = "Complete name", dataType = "varchar",  position = 1) 
+	@JsonProperty("name")
 	private String name;
+	
+	@ApiModelProperty(notes = "Number of Items", dataType = "integer",  position = 2) 
+	@JsonProperty("quantity")
 	private Integer qty;
+	
+	@ApiModelProperty(notes = "item Price", dataType = "decimal",  position = 3) 
+	@JsonProperty("price")
 	private Float price;
+	
+	@ApiModelProperty(notes = "Location", dataType = "varchar",  position = 4) 
+	@JsonProperty("location")
 	private String location;
+	
+	@ApiModelProperty(notes = "Description", dataType = "varchar",  position = 5) 
+	@JsonProperty("description")
 	private String description;
+	
+	@ApiModelProperty(notes = "Status", dataType = "varchar",  position = 6)
+	@JsonProperty("status")
 	private String status;
+	
+	@ApiModelProperty(notes = "Delivery Date", dataType = "date", position = 7) 
+	@JsonProperty("bday")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy HH:mm", locale = "de")
 	private LocalDateTime deliveryDate;
+	
+	@ApiModelProperty(notes = "Type", dataType = "varchar", position = 8) 
+	@JsonProperty("type")
 	private String type;
+	
+	@ApiModelProperty(notes = "Owner", dataType = "varchar", position = 9) 
+	@JsonProperty("owner")
 	private String owner;
 }
